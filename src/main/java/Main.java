102255937ce18b8048f4ca795b96c4a96710dded
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2023-01-29 21:58:58 +0200

    added second congratulations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello mates!";
-        System.out.println(message);
+        System.out.println("Hello world");
+        System.out.println("Hello mates!");
     }
 }
